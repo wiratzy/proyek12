f8b55f76f50530f82079d64b686e8c78d4ea96de
riskkif <riskisam78@gmail.com>
2023-11-21 22:52:57 +0700

    tes img

--- resources/views/layoutkaryawan/footer.blade.php
+++ resources/views/layoutkaryawan/footer.blade.php
@@ -2,7 +2,7 @@
 <!-- /.content-wrapper -->
 <footer class="main-footer">
 <strong>Copyright &copy; 2014-2021 <a
-href="https://adminlte.io"></a>.</strong>
+href="https://adminlte.io">f</a>.</strong>
 All rights reserved.
 <div class="float-right d-none d-sm-inline-block">
 <b>Version</b> 3.2.0
