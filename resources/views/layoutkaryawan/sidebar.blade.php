4f0c1ce7af9f405966651e8cb01eddf386208269
riskkif <riskisam78@gmail.com>
2023-11-21 23:05:42 +0700

    f

--- resources/views/layoutkaryawan/sidebar.blade.php
+++ resources/views/layoutkaryawan/sidebar.blade.php
@@ -10,25 +10,6 @@ alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity:
 .8">
 <span class="brand-text font-weight-light">CRUD LARAVEL</span>
 </a> -->
-<<<<<<< HEAD
-<!-- Sidebar -->
-<div class="sidebar">
-<!-- Sidebar user panel (optional) -->
-<div class="user-panel mt-3 pb-3 mb-3 d-flex">
-<div class="image">
-<img src="{{ asset('adminlte') }}/dist/img/user2-160x160.jpg"
-class="img-circle elevation-2" alt="User Image">
-</div>
-<div class="info">
-<a href="/profil" class="d-block text-decoration-none">{{ auth()->user()->name }}</a>
-</div>
-</div>
-<!-- Sidebar Menu -->
-<nav class="mt-2">
-<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview"
-role="menu" data-accordion="false">
-<!-- Add icons to the links using the .nav-icon class
-=======
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
